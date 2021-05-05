82ac3a1e40bb6ad7c84614d7b312efee0f0720e6
mosesmgk <mlmamoses2@gmail.com>
2021-05-04 17:36:13 -0700

    commit

---
+++ cst438week-homework1/src/main/java/cst438hw1/Cst438weekHomework1Application.java
@@ -0,0 +1,13 @@
+package cst438hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438weekHomework1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438weekHomework1Application.class, args);
+	}
+
+}
---
+++ cst438week-homework1/src/test/java/cst438hw1/Cst438weekHomework1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438weekHomework1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
